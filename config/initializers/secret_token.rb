9128ccaed1570eca355ccb75f94436b42248ca0d
Mai <catty_chan16@yahoo.com>
2015-02-02 18:09:33 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TspApp::Application.config.secret_key_base = '281447ac626c268aa8e913887e5f6b5b83c84933a7b3a10949047907137bec46042699a3bdf495b5f9e51ac0cad36ea8bb422bed9c03e71dc5aab05f025a4f33'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TspApp::Application.config.session_store :cookie_store, key: '_tsp_app_session'
